aa2c9f2e7ba7dfaee9111f86fcb36cf8bbd99bdb
anviam <raj.kumar@anviam.com>
2024-05-16 17:37:04 +0530

    layout-fixes

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,24 +1,3 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-
-    <!-- Scripts -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-    
-    @include('include.styles')
-    
-</head>
-<body>
 
     <!-- ======= Header ======= -->
     <header id="header" class="header fixed-top d-flex align-items-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,35 @@
-@include('include.header')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@stack('styles')
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'KRTY') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.bunny.net">
 
-    <!-- Scripts -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-</head>
-<body>
+        @include('include.styles')
+        
+        @stack('styles')
+        
+        
+    </head>
+    <body>
 
-    <div id="app">
-
-        {{-- <main class="py-4"> --}}
+        @if (Auth::check())
+            @include('include.header')
+        @endif
+        
+        <div id="app">
             @yield('content')
-        {{-- </main> --}}
-    </div>
+        </div>
 
-@include('include.footer')
+    @include('include.footer')
 
-@stack('scripts')
+    </body>
+
+</html>
