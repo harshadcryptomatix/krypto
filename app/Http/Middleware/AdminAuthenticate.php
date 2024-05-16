2e79dfe1fdb56b7b36fc922af710704ee6724b9a
pankaj dheer <pankaj.dheer@payomatix.com>
2024-05-16 16:17:13 +0530

    admin login,logout,admin middleware,seeder

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthenticate
+{
+    public function handle(Request $request, Closure $next)
+    {
+       
+        if (Auth::guard('admin')->check()) {
+            $user = auth()->guard('admin')->user();
+        
+            if(!$user->status){
+                return redirect()->route('admin.login')->with('error','Your account is suspended');
+            } 
+            return $next($request);
+        }
+
+        return redirect()->route('admin.login'); // Redirect to admin login page
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::post('/email/verification-notification', function (Request $request) {
  
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+
+//admin routes attach with admin.php file
+Route::prefix('admin')->group(function () {
+    require base_path('routes/admin.php');
+});
